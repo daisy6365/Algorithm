630062c7df26e56c0666d7a36866b67fc3a6b59b
daisy6365 <gksekqls9808@inu.ac.kr>
2023-01-01 18:03:02 +0900

    add : 0101

--- Programmers/src/예산.java
+++ Programmers/src/예산.java
@@ -1,6 +1,23 @@
+import java.util.Arrays;
+
 public class 예산 {
     public static int solution(int[] d, int budget) {
         int answer = 0;
+        Arrays.sort(d);
+        for (int i = 0; i < d.length; i++) {
+            if(d[i] <= budget){
+                budget -= d[i];
+                answer++;
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < d.length; i++) {
+            sum += d[i];
+            if(sum <= budget){
+                answer++;
+            }
+            else{ break;}
+        }
         return answer;
     }
 
---
+++ Programmers/src/피보나치수.java
@@ -0,0 +1,19 @@
+public class 피보나치수 {
+    public static void main(String[] args) {
+        System.out.println(solution(3));
+    }
+    public static int solution(int n) {
+        int answer = 0;
+        if(n == 1 || n == 0) return 1;
+        int num_1 = 0;
+        int num_2 = 1;
+
+        for (int i = 2; i <= n; i++) {
+            answer = (num_1 + num_2) % 1234567;
+            num_1 = num_2;
+            num_2 = answer;
+        }
+        return answer;
+    }
+
+}
