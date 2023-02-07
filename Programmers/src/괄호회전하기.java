b4c9f430f158ead9606b613b775ffcd350dcd975
한다빈 <gksekqls9808@naver.com>
2023-02-07 23:26:38 +0900

    add : 0207

--- Programmers/src/괄호회전하기.java
+++ Programmers/src/괄호회전하기.java
@@ -1,7 +1,6 @@
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
+import java.util.Stack;
 
 public class 괄호회전하기 {
     public static void main(String[] args) {
@@ -16,52 +15,42 @@ public class 괄호회전하기 {
 
 
         // Si만큼 회전
-        flag: for (int i = 0; i < s.length(); i++) {
-            Queue<Character> queue = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            Stack<Character> stack = new Stack<>();
+
             String temp_s = "";
             temp_s += s.substring(i);
             temp_s += s.substring(0, i);
 
-            boolean flag = true;
-            System.out.println("!!검사시작!! = " + temp_s);
-            int j = 0;
-            while(true){
-                if(queue.isEmpty()) break;
-                if(j == temp_s.length()) j = 0;
-                if(temp_s.charAt(j) == '('){
-                    queue.add('(');
-                }
-                else if(temp_s.charAt(j) == '{'){
-                    queue.add('{');
-                }
-                else if(temp_s.charAt(j) == '['){
-                    queue.add('[');
+            for (int j = 0; j < temp_s.length() ; j++) {
+                if(temp_s.charAt(j) == '(' || temp_s.charAt(j) == '{' || temp_s.charAt(j) == '[') {
+                    stack.push(temp_s.charAt(j));
                 }
                 else {
-                    if(queue.isEmpty()) {
-                        continue flag;
+                    if(!stack.isEmpty()){
+                        if(temp_s.charAt(j) == ')'){
+                            checkCharactor(stack, '(');
+                        }
+                        else if(temp_s.charAt(j) == '}'){
+                            checkCharactor(stack, '{');
+                        }
+                        else if(temp_s.charAt(j) == ']'){
+                            checkCharactor(stack, '[');
+                        }
                     }
-                    else {
-                        char temp = queue.peek();
-                        if(temp == '(' && temp_s.charAt(j) == ')'){
-                            queue.poll();
-                        }
-                        else if(temp == '{' && temp_s.charAt(j) == '}'){
-                            queue.poll();
-                        }
-                        else if(temp == '[' && temp_s.charAt(j) == ']'){
-                            queue.poll();
-                        }
-                        else {
-                            flag = false;
-                        }
+                    else{
+                        stack.push(s.charAt(j));
                     }
                 }
             }
-            System.out.println("queue = " + queue);
-            if(queue.isEmpty() && flag){answer++;}
-            System.out.println("answer = "+answer);
+            if(stack.isEmpty()){answer++;}
         }
         return answer;
+    }
+
+    private static void checkCharactor(Stack<Character> stack, char check_char){
+        if(check_char == stack.peek()){
+            stack.pop();
+        }
     }
 }
