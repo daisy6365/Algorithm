c2fc2f409b8a3256463debec2a36647364f290bc
한다빈 <gksekqls9808@naver.com>
2023-02-20 18:15:00 +0900

    add : 0220 - Programmers 위장

--- Programmers/src/위장.java
+++ Programmers/src/위장.java
@@ -4,22 +4,33 @@ public class 위장 {
     public static void main(String[] args) {
         String[][] clothes1 = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
         String[][] clothes2 = {{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}};
+        String[][] clothes3 = {{"a", "headgear"}, {"b", "headgear"}, {"c", "eyewear"}, {"d", "eyewear"}, {"e", "face"}, {"f", "face"}}; /// 26
         System.out.println(solution(clothes1));
         System.out.println(solution(clothes2));
+        System.out.println(solution(clothes3));
     }
     public static int solution(String[][] clothes) {
-        int answer = 0;
-        int temp_duplicate = 0;
-        HashMap<String, String> hashMap = new HashMap<>();
-        for (int i = 0; i < clothes.length; i++) {
-            if(!hashMap.isEmpty() && !hashMap.containsValue(clothes[i][1])){
-                temp_duplicate++;
-            }
+        int answer = 1;
+        HashMap<String, Integer> hashMap = new HashMap<>();
 
-            hashMap.put(clothes[i][0], clothes[i][1]);
+        for (int i = 0; i < clothes.length; i++) {
+            if(hashMap.containsKey(clothes[i][1])){
+                // 중복 -> 값++
+                int temp_value = hashMap.get(clothes[i][1]);
+                hashMap.replace(clothes[i][1], temp_value+1);
+            }
+            else{
+                hashMap.put(clothes[i][1], 1);
+            }
         }
 
-        answer = hashMap.size() + temp_duplicate * (hashMap.size() - temp_duplicate);
+        // 키의 갯수 + 1(아무것도 안하는 것의 경우의 수)
+        for (String s: hashMap.keySet()) {
+            answer *= (hashMap.get(s) + 1);
+        }
+
+        // 걍 모든걸 안한는 것의 경우 1--
+        answer--;
 
         return answer;
     }
