31a420f5c7578851a830789eb9125bb2eca2e90d
한다빈 <gksekqls9808@naver.com>
2023-02-23 15:44:18 +0900

    add : 0223 - BOJ 1406 에디터

--- BOJ/src/BOJ_1406_에디터.java
+++ BOJ/src/BOJ_1406_에디터.java
@@ -2,49 +2,103 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
+// 다시 풀이
 public class BOJ_1406_에디터 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine(); // 주어진 문자열
         StringBuilder sb = new StringBuilder();
 
-        String s = br.readLine();
-        int N = Integer.parseInt(br.readLine());
-        Stack<Character> left = new Stack<>();
-        Stack<Character> right = new Stack<>();
+        int M = Integer.parseInt(br.readLine()); // 명령어
 
+        Stack<Character> cursor_left = new Stack<>();
+        Stack<Character> cursor_right = new Stack<>();
 
-        for (int i = 0; i < s.length(); i++) {
-            left.push(s.charAt(i));
+        for (int i = 0; i < word.length() ; i++) {
+            cursor_left.push(word.charAt(i));
         }
 
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            String cmd = st.nextToken();
-            if (cmd.equals("L")) { // 왼쪽 옮기기
-                if (!left.isEmpty()) {
-                    right.push(left.pop());
+        for (int i = 0; i < M; i++) {
+            String command = br.readLine();
+
+            switch (command.charAt(0)) {
+                case 'L' :{ // 왼쪽
+                    if (!cursor_left.isEmpty()){
+                        cursor_right.push(cursor_left.pop());
+                    }
+                    break;
                 }
-            } else if (cmd.equals("D")) { // 오른쪽 옮기기
-                if (!right.isEmpty()) {
-                    left.push(right.pop());
+                case 'D' :{ // 오른쪽
+                    if (!cursor_right.isEmpty()){
+                        cursor_left.push(cursor_right.pop());
+                    }
+                    break;
                 }
-            } else if (cmd.equals("B")) { // 왼쪽 문자 삭제
-                if (!left.isEmpty()) {
-                    left.pop();
+                case 'B' :{ // 커서기준 왼쪽문자 삭제
+                    if (!cursor_left.isEmpty()){
+                        cursor_left.pop();
+                    }
+                    break;
                 }
-            } else { // P -> P 다음 오는 문자 넣기
-                left.push(st.nextToken().charAt(0));
+                case 'P' :{ // 문자 커서기준 왼쪽에 추가
+                    cursor_left.push(command.charAt(2));
+                    break;
+                }
             }
-        }
 
-        for (Character c : left) {
+        }
+        for (char c: cursor_left) {
             sb.append(c);
         }
-        while (!right.isEmpty()) {
-            sb.append(right.pop());
+
+        while(true){
+            if(cursor_right.isEmpty()) break;
+            sb.append(cursor_right.pop());
         }
+
         System.out.println(sb);
     }
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//
+//        String s = br.readLine();
+//        int N = Integer.parseInt(br.readLine());
+//        Stack<Character> left = new Stack<>();
+//        Stack<Character> right = new Stack<>();
+//
+//
+//        for (int i = 0; i < s.length(); i++) {
+//            left.push(s.charAt(i));
+//        }
+//
+//        for (int i = 0; i < N; i++) {
+//            StringTokenizer st = new StringTokenizer(br.readLine());
+//            String cmd = st.nextToken();
+//            if (cmd.equals("L")) { // 왼쪽 옮기기
+//                if (!left.isEmpty()) {
+//                    right.push(left.pop());
+//                }
+//            } else if (cmd.equals("D")) { // 오른쪽 옮기기
+//                if (!right.isEmpty()) {
+//                    left.push(right.pop());
+//                }
+//            } else if (cmd.equals("B")) { // 왼쪽 문자 삭제
+//                if (!left.isEmpty()) {
+//                    left.pop();
+//                }
+//            } else { // P -> P 다음 오는 문자 넣기
+//                left.push(st.nextToken().charAt(0));
+//            }
+//        }
+//
+//        for (Character c : left) {
+//            sb.append(c);
+//        }
+//        while (!right.isEmpty()) {
+//            sb.append(right.pop());
+//        }
+//        System.out.println(sb);
+//    }
 }
