ff69de4382a46f2a98a5b57f3c13972cae1b85ac
한다빈 <gksekqls9808@naver.com>
2023-03-07 16:33:43 +0900

    add : 0307 - BOJ 2615 오목

--- BOJ/src/BOJ_2615_오목.java
+++ BOJ/src/BOJ_2615_오목.java
@@ -6,8 +6,7 @@ import java.util.StringTokenizer;
 public class BOJ_2615_오목 {
     static int[][] board = new int[19][19];
     // 우 하 우하 우상
-    static int[][] direction = {{0, 1}, {1, 0}, {1, 1}, {-1, 1}}; // -> 1, -1을 곱해야 양방 탐색
-    static int result_color, result_col, result_row;
+    static int[][] direction = {{0, 1}, {1, 0}, {1, 1}, {-1, 1}};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -19,14 +18,16 @@ public class BOJ_2615_오목 {
             }
         }
 
-        for (int i = 0; i < 19; i++) {
-            for (int j = 0; j < 19; j++) {
+        // *@*@ 이게 키 포인트. *@*@
+        // 발견하려는것의 가장 왼쪽의 있는것을 확인 & 출력
+        for (int j = 0; j < 19; j++) {
+            for (int i = 0; i < 19; i++) {
                 // 1,2를 만나면 오목 체크 (탐색)
                 if(board[i][j] != 0){
                     // 오목 맞을 때
                     if(is_condition(i, j)){
-                        System.out.println(result_color);
-                        System.out.println(result_col + " " + result_row);
+                        System.out.println(board[i][j]);
+                        System.out.println((i+1) + " " + (j+1));
                         return;
                     }
 
@@ -35,34 +36,32 @@ public class BOJ_2615_오목 {
         }
         System.out.println(0);
     }
-    private static boolean is_condition(int i, int j){
-        for(int d = 0; d < 4; d++) {
-            int count = 1;
-            count += isCheck(i, j, direction[d][0], direction[d][1]);
-            count += isCheck(i, j, direction[d][0] * -1, direction[d][1] * -1);
 
-            if(count == 5) {
-                result_color = board[i][j];
-                result_col = i + 1;
-                result_row = j + 1;
+    private static boolean is_condition(int nowi, int nowj){
+        for(int d = 0; d < 4; d++){
+            int count = 1;
+            count += check_omock(nowi, nowj, direction[d][0], direction[d][1]);
+            count += check_omock(nowi, nowj, direction[d][0] * -1, direction[d][1] * -1);
+
+            if(count == 5){
                 return true;
             }
         }
         return false;
     }
 
-    private static int isCheck(int i, int j , int di, int dj){
-        int nexti = i + di;
-        int nextj = j + dj;
+    private static int check_omock(int nowi, int nowj, int di, int dj){
+        int nexti = nowi + di;
+        int nextj = nowj + dj;
         int count = 0;
 
-        for (int k = 0; k < 19; k++) {
-            if (nexti >= 0 && nexti < 19 && nextj >= 0 && nextj < 19 && board[nexti][nextj] == board[i][j]) {
+        for(int i = 0; i < 19 ; i++){
+            if(nexti >= 0 && nexti < 19 & nextj >= 0 && nextj < 19 && board[nowi][nowj] == board[nexti][nextj]){
                 count++;
                 nexti += di;
                 nextj += dj;
             }
-            else {
+            else{
                 return count;
             }
         }
