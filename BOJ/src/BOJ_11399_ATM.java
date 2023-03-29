e41713863eb5226184124e9fc4a8738d5bb0dce9
한다빈 <gksekqls9808@naver.com>
2023-03-29 21:36:17 +0900

    add : 0329 - BOJ 11399 ATM

--- BOJ/src/BOJ_11399_ATM.java
+++ BOJ/src/BOJ_11399_ATM.java
@@ -8,19 +8,39 @@ public class BOJ_11399_ATM {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
+        int[] people = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int [] arr = new int[N];
-        int wait_time =0;
-        int total_time = 0;
+
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            people[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr);
-        for (int i = 0; i < N; i++) {
-            wait_time += arr[i];
-            total_time += wait_time;
+
+        Arrays.sort(people);
+        int result = people[0];
+        for (int i = 1; i < N; i++) {
+            people[i] = people[i-1] + people[i];
+            result += people[i];
         }
-        System.out.println(total_time);
+
+        System.out.println(result);
     }
 }
+
+//public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int N = Integer.parseInt(br.readLine());
+//
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        int [] arr = new int[N];
+//        int wait_time =0;
+//        int total_time = 0;
+//        for (int i = 0; i < N; i++) {
+//            arr[i] = Integer.parseInt(st.nextToken());
+//        }
+//        Arrays.sort(arr);
+//        for (int i = 0; i < N; i++) {
+//            wait_time += arr[i];
+//            total_time += wait_time;
+//        }
+//        System.out.println(total_time);
+//    }
