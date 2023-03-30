21abbbdf2f3f79d4facdd820fd906f733fb503e0
한다빈 <gksekqls9808@naver.com>
2023-03-30 23:44:07 +0900

    add : 0330 - BOJ 4358 생태학

--- BOJ/src/BOJ_4358_생태학.java
+++ BOJ/src/BOJ_4358_생태학.java
@@ -1,35 +1,27 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.sql.SQLOutput;
 import java.util.*;
 
 public class BOJ_4358_생태학 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Map<String, Integer> tree_list = new HashMap<>();
-        int sum =1;
+        Scanner sc = new Scanner(System.in);
+        Map<String, Integer> map = new TreeMap<>();
+        int count = 0;
 
-        String s = br.readLine();
-        while (s.length() != 0){
-            if(!tree_list.containsKey(s)){
-                tree_list.put(s,1);
+        while(sc.hasNextLine()){
+            String temp_s = sc.nextLine();
+            if(map.containsKey(temp_s)){
+                map.replace(temp_s, map.get(temp_s)+1);
             }
-            else if (tree_list.containsKey(s)){
-                tree_list.put(s,tree_list.get(s)+1);
+            else{
+                map.put(temp_s, 1);
             }
-            sum++;
-            s = br.readLine();
-
+            count++;
         }
-        StringBuilder sb = new StringBuilder();
-        List<String> keys = new ArrayList<>(tree_list.keySet());
-        Collections.sort(keys);
-        for (String key : keys) {
-            double per = (tree_list.get(key) * 100.0) / sum;
-            sb.append(key + " " + String.format("%.4f", per) + "\n");
-        }
-        System.out.println(sb);
 
+        for(String key : map.keySet()) {
+            double num = (double)(map.get(key) * 100) / count;
+
+            System.out.println(key + " " + String.format("%.4f", num));
+        }
     }
 }
