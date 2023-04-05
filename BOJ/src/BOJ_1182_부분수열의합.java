72e04db42e4938a5d61838dcce332ba3542df5a7
한다빈 <gksekqls9808@naver.com>
2023-04-05 23:57:25 +0900

    add : 0405 - BOJ 1182 부분수열의합

--- BOJ/src/BOJ_1182_부분수열의합.java
+++ BOJ/src/BOJ_1182_부분수열의합.java
@@ -1,49 +1,88 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BOJ_1182_부분수열의합 {
-    static int N,S;
-    static int result;
+    static int N, S, count;
+    static int[] arr;
     static boolean[] isSelected;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st= new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         S = Integer.parseInt(st.nextToken());
-
-        int [] num_arr = new int[N];
+        arr = new int[N];
         isSelected = new boolean[N];
-        st =  new StringTokenizer(br.readLine());
-        for (int n = 0; n < N; n++) {
-            num_arr[n] = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        recursion(0,num_arr,0);
-        System.out.println(result-1);
+        subSet(0, 0);
+        System.out.println(count);
     }
-    static void recursion(int cnt, int num_arr[], int sum) {
-        if (cnt == N) {
-            int falseCnt = 0;
-            for (int i = 0; i < N; i++) {
-                if (!isSelected[i]) {
-                    falseCnt++;
+
+    private static void subSet(int sum, int index){
+        if(index == N){
+            int falseCount = 0;
+
+            for(int i = 0; i < N ; i++){
+                if(!isSelected[i]){
+                    falseCount++;
                 }
             }
-            if (falseCnt != N) {
-                if (sum == S) {
-                    result++;
+            if(falseCount != N){
+                if(sum == S){
+                    count++;
                 }
-
             }
             return;
         }
-        isSelected[cnt] = true;
-        recursion(cnt + 1, num_arr, sum + num_arr[cnt]);
-        isSelected[cnt] = false;
-        recursion(cnt + 1, num_arr, sum);
+        isSelected[index] = true;
+        subSet(sum + arr[index], index+1);
+        isSelected[index] = false;
+        subSet(sum , index + 1);
     }
-
 }
+//static int N,S;
+//    static int result;
+//    static boolean[] isSelected;
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        N = Integer.parseInt(st.nextToken());
+//        S = Integer.parseInt(st.nextToken());
+//
+//        int [] num_arr = new int[N];
+//        isSelected = new boolean[N];
+//        st =  new StringTokenizer(br.readLine());
+//        for (int n = 0; n < N; n++) {
+//            num_arr[n] = Integer.parseInt(st.nextToken());
+//        }
+//
+//        recursion(0,num_arr,0);
+//        System.out.println(result-1);
+//    }
+//    static void recursion(int cnt, int num_arr[], int sum) {
+//        if (cnt == N) {
+//            int falseCnt = 0;
+//            for (int i = 0; i < N; i++) {
+//                if (!isSelected[i]) {
+//                    falseCnt++;
+//                }
+//            }
+//            if (falseCnt != N) {
+//                if (sum == S) {
+//                    result++;
+//                }
+//
+//            }
+//            return;
+//        }
+//        isSelected[cnt] = true;
+//        recursion(cnt + 1, num_arr, sum + num_arr[cnt]);
+//        isSelected[cnt] = false;
+//        recursion(cnt + 1, num_arr, sum);
+//    }
