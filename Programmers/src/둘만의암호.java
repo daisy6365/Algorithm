a48c3bb7186423f57467e46c7a948a5c5674ce6a
한다빈 <gksekqls9808@naver.com>
2023-04-12 16:07:41 +0900

    add : 0412 - Programmers 둘만의암호

---
+++ Programmers/src/둘만의암호.java
@@ -0,0 +1,31 @@
+public class 둘만의암호 {
+    public static void main(String[] args) {
+        System.out.println(solution("aukks" , "wbqd", 	5));
+        System.out.println(solution("zzzzz" , "a", 	1));
+    }
+
+    public static String solution(String s, String skip, int index) {
+        StringBuilder answer = new StringBuilder();
+
+        for(int i = 0; i < s.length() ; i++){
+            char change_s = s.charAt(i);
+            int count = 0;
+            while(true){
+                if(count == index) break;
+                // change_s 바꾸기
+                if(change_s == 'z'){
+                    change_s = 'a';
+                }
+                else {
+                    change_s += 1;
+                }
+                if(skip.indexOf(change_s) < 0){
+                    count++;
+                }
+            }
+            answer.append(change_s);
+        }
+
+        return answer.toString();
+    }
+}
