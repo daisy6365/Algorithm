bfbedbbb46eb4cdf21cc738d51532adaed905c3e
한다빈 <gksekqls9808@naver.com>
2023-04-12 14:33:29 +0900

    add : 0412 - BOJ 15656 N과M7

---
+++ BOJ/src/BOJ_15656_N과M7.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class BOJ_15656_N과M7 {
+    static int N, M;
+    static int[] num, arr;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        num = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N ; i++){
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(num);
+
+        arr = new int[M];
+        backtracking(0);
+        System.out.println(sb);
+    }
+
+    private static void backtracking(int index) {
+        if(index == M){
+            for(int i = 0; i < M; i++){
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i = 0; i < N ; i++){
+            arr[index] = num[i];
+            backtracking(index+1);
+        }
+    }
+}
