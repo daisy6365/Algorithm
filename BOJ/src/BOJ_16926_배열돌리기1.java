1e557cb8f1b0349192523e1c23827310ccf267e1
한다빈 <gksekqls9808@naver.com>
2023-04-19 16:53:15 +0900

    add : 0419 - BOJ 16926 배열돌리기1

--- BOJ/src/BOJ_16926_배열돌리기1.java
+++ BOJ/src/BOJ_16926_배열돌리기1.java
@@ -1,57 +1,110 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class BOJ_16926_배열돌리기1 {
+    static int N, M, R;
+    static int[][] arr;
+    static int[][] copy;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int R = Integer.parseInt(st.nextToken());
-        int arr[][] = new int[N][M];
-        int copy[][] = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < M; j++) {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+
+        arr = new int[N][M];
+        copy = new int[N][M];
+
+        for(int i = 0 ; i < N ; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0 ; j < M ; j++){
                 arr[i][j] = Integer.parseInt(st.nextToken());
                 copy[i][j] = arr[i][j];
             }
         }
 
-        int min = Math.min(N, M);
-        while (R-- > 0) {
-            for (int circle = 0; circle < min / 2; circle++) {
-                int li = circle;
-                int lj = circle;
-                int ri = N - 1 - circle;
-                int rj = M - 1 - circle;
-                for (int i = lj; i < rj; i++) {
-                    arr[li][i] = copy[li][i + 1];
+        int min = Math.min(N, M) / 2; // min(N, M) mod 2 = 0
+        for(int r = 0 ; r < R ; r++){
+            for(int i = 0 ; i < min ; i++){
+                int temp = arr[i][i]; // 0,0 1,1 2,2
+                // 상
+                for(int j = i+1 ; j < M- i ; j++){
+                    arr[i][j-1] = arr[i][j];
                 }
-                for (int i = li + 1; i < ri + 1; i++) {
-                    arr[i][lj] = copy[i - 1][lj];
+                // 우
+                for(int j = i+1 ; j < N - i ; j++){
+                    arr[j-1][M-1-i] = arr[j][M-1-i];
                 }
-                for (int i = lj + 1; i < rj + 1; i++) {
-                    arr[ri][i] = copy[ri][i - 1];
+                // 하
+                for(int j = M-2-i ; j >= i ; j--){
+                    arr[N-1-i][j+1] = arr[N-1-i][j];
                 }
-                for (int i = ri - 1; i >= li; i--) {
-                    arr[i][rj] = copy[i + 1][rj];
+                // 좌
+                for(int j = N-2-i ; j >= i ; j--){
+                    arr[j+1][i] = arr[j][i];
                 }
-            }
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    copy[i][j] = arr[i][j];
-                }
+
+                // 바로 아래에 넣기
+                arr[i+1][i] = temp;
             }
         }
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                System.out.print(arr[i][j] + " ");
+
+        for(int i = 0 ; i < N ; i++){
+            for(int j =0 ; j < M ; j++){
+                System.out.print(arr[i][j] +" ");
             }
             System.out.println();
         }
 
     }
 }
+//public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//        int N = Integer.parseInt(st.nextToken());
+//        int M = Integer.parseInt(st.nextToken());
+//        int R = Integer.parseInt(st.nextToken());
+//        int arr[][] = new int[N][M];
+//        int copy[][] = new int[N][M];
+//        for (int i = 0; i < N; i++) {
+//            st = new StringTokenizer(br.readLine(), " ");
+//            for (int j = 0; j < M; j++) {
+//                arr[i][j] = Integer.parseInt(st.nextToken());
+//                copy[i][j] = arr[i][j];
+//            }
+//        }
+//
+//        int min = Math.min(N, M);
+//        while (R-- > 0) {
+//            for (int circle = 0; circle < min / 2; circle++) {
+//                int li = circle;
+//                int lj = circle;
+//                int ri = N - 1 - circle;
+//                int rj = M - 1 - circle;
+//                for (int i = lj; i < rj; i++) {
+//                    arr[li][i] = copy[li][i + 1];
+//                }
+//                for (int i = li + 1; i < ri + 1; i++) {
+//                    arr[i][lj] = copy[i - 1][lj];
+//                }
+//                for (int i = lj + 1; i < rj + 1; i++) {
+//                    arr[ri][i] = copy[ri][i - 1];
+//                }
+//                for (int i = ri - 1; i >= li; i--) {
+//                    arr[i][rj] = copy[i + 1][rj];
+//                }
+//            }
+//            for (int i = 0; i < N; i++) {
+//                for (int j = 0; j < M; j++) {
+//                    copy[i][j] = arr[i][j];
+//                }
+//            }
+//        }
+//        for (int i = 0; i < N; i++) {
+//            for (int j = 0; j < M; j++) {
+//                System.out.print(arr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+//
+//    }
